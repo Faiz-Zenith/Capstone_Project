381ca955d1cc85572c42d31ba3c9b94975dd4328
M.Faiz Amanullah <faiz010504@gmail.com>
2024-11-24 18:14:15 +0700

    loginandregister

--- app/src/main/java/com/muhammadiyah/becekcapstoneproject/register/RegisterActivity.kt
+++ app/src/main/java/com/muhammadiyah/becekcapstoneproject/register/RegisterActivity.kt
@@ -2,29 +2,20 @@ package com.muhammadiyah.becekcapstoneproject.register
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.muhammadiyah.becekcapstoneproject.R
+import com.muhammadiyah.becekcapstoneproject.databinding.ActivityRegisterBinding
 import com.muhammadiyah.becekcapstoneproject.login.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        val tvHaveAccount = findViewById<TextView>(R.id.tv_have_account)
-        tvHaveAccount.setOnClickListener {
-            // Navigate to LoginActivity
+        // Set up navigation to LoginActivity
+        binding.tvHaveAccount.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
